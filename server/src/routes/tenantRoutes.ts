bc31b54cd7a64b404298213237722eeae154ee3d
Christian Handoko <christianhandoko46@gmail.com>
2025-04-20 17:06:42 +0700

    backend complete

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { createTenant, getTenant, updateTenant } from "../controllers/tenantControllers";
+import {
+  addFavoriteProperty,
+  createTenant,
+  getCurrentResidences,
+  getTenant,
+  removeFavoriteProperty,
+  updateTenant,
+} from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
 router.post("/", createTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
+router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
+router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
 
 export default router;
