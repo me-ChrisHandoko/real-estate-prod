7b00a5ed29ed2a039e779b8c602c7b0968f5d7e7
Christian Handoko <christianhandoko46@gmail.com>
2025-05-24 22:33:04 +0700

    add config

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "example.com", port: "", pathname: "/**" }],
+    remotePatterns: [
+      { protocol: "https", hostname: "example.com", port: "", pathname: "/**" },
+      { protocol: "https", hostname: "*.amazonaws.com", port: "", pathname: "/**" },
+    ],
   },
 };
 
